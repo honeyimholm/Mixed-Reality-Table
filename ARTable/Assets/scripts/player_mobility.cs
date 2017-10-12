e1d92a6406df0c503e3a33424d1174a75a3d5da8
honeyimholm <honeyimholm@gmail.com>
2017-10-12 15:43:18 -0400

    pushing current files/folders

---
+++ ARTable/Assets/scripts/player_mobility.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class player_mobility : MonoBehaviour {
+
+    public float speed=1;
+
+    void FixedUpdate()
+    {
+        //todo:
+        var mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Quaternion rot = Quaternion.LookRotation(transform.position - mousePosition, Vector3.forward);
+        transform.rotation = rot;
+        transform.eulerAngles = new Vector3(0, 0, transform.eulerAngles.z);
+        GetComponent<Rigidbody2D>().angularVelocity = 0;
+        float input = Input.GetAxis("Vertical");
+        //GetComponent<Rigidbody2D>().AddForce(gameObject.transform.up * speed * input);
+    }
+    void Update() { }
+}
